fb1782369f6809c9d8ce54c96827f806cedd0f07
Daniel Zadok <zadok.daniel@gmail.com>
2026-04-23 21:15:41 +0300

    django urls

--- 2026-04-16 W300125ER/class_project/lec1/lec1/urls.py
+++ 2026-04-16 W300125ER/class_project/lec1/lec1/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from cards.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cards/', index)
+    path('cards/', include("cards.urls"))
 ]
