b21977844ca528a7ea2527ff5006620bba457243
Daniel Zadok <zadok.daniel@gmail.com>
2026-05-14 20:13:01 +0300

    drf class based API views

--- 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
+++ 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
@@ -1,38 +1,17 @@
-from django.shortcuts import render
-# from django.http import HttpRequest, JsonResponse
 from .models import Post
 from .serializer import PostSerializer
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.mixins import *
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 
-class PostViews(GenericAPIView, CreateModelMixin, ListModelMixin):
+class PostViews(ListCreateAPIView):
     name = 'PostViews'
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request: Request):
-        return self.list(request)
 
-    def post(self, request: Request):
-        return self.create(request)
-
-
-class PostViewByPk(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class PostViewByPk(RetrieveUpdateDestroyAPIView):
     name = 'PostViewsByPk'
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request: Request, pk: int):
-        return self.retrieve(request, pk)
-
-    def put(self, request: Request, pk: int):
-        return self.update(request, pk)
-
-    def delete(self, request: Request, pk: int):
-        return self.destroy(request, pk)
