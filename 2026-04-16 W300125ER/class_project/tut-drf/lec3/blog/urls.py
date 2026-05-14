42013a3e9816d77d457c7b6e317d07e35e17875b
Daniel Zadok <zadok.daniel@gmail.com>
2026-05-14 20:55:04 +0300

    drf viewset form the data actions prespective

--- 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/urls.py
+++ 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('pictures', views.PicturesViewSet, 'pictures')
+
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('posts/', views.PostViews.as_view()),
     path('posts/<int:pk>/', views.PostViewByPk.as_view())
 ]
--- 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
+++ 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
@@ -1,8 +1,34 @@
 from .models import Post
 from .serializer import PostSerializer
-from rest_framework.request import Request
-from rest_framework.mixins import *
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+class PicturesViewSet(ViewSet):
+
+    def list(self, request):
+        return Response('list')
+
+    def create(self, request):
+        return Response('create')
+
+    def retrieve(self, request, pk=None):
+        return Response('retrieve')
+
+    def update(self, request, pk=None):
+        return Response('update')
+
+    def partial_update(self, request, pk=None):
+        return Response('partial_update')
+
+    def destroy(self, request, pk=None):
+        return Response('destroy')
+
+    @action(detail=True, methods=['get'])
+    def thumbnail(self, request, pk=None):
+        return Response('thumbnail')
 
 
 class PostViews(ListCreateAPIView):
