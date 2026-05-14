b692d41b7deb80d26ad67f115209e9afd4260e36
Daniel Zadok <zadok.daniel@gmail.com>
2026-05-14 21:11:17 +0300

    drf modelviewset the best of both worlds

--- 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
+++ 2026-04-16 W300125ER/class_project/tut-drf/lec3/blog/views.py
@@ -1,30 +1,14 @@
 from .models import Post
 from .serializer import PostSerializer
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
 
-class PicturesViewSet(ViewSet):
-
-    def list(self, request):
-        return Response('list')
-
-    def create(self, request):
-        return Response('create')
-
-    def retrieve(self, request, pk=None):
-        return Response('retrieve')
-
-    def update(self, request, pk=None):
-        return Response('update')
-
-    def partial_update(self, request, pk=None):
-        return Response('partial_update')
-
-    def destroy(self, request, pk=None):
-        return Response('destroy')
+class PicturesViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     @action(detail=True, methods=['get'])
     def thumbnail(self, request, pk=None):
